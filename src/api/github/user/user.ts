e1e156fda5fe15ea0756a978ebeea5cbf53f990c
byseop <byseop@gmail.com>
2020-02-04 14:06:34 +0900

    UPDATE: user api base url

--- src/api/github/user/user.ts
+++ src/api/github/user/user.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export async function getUser(token: string) {
   const response = await axios({
     method: 'get',
+    baseURL: 'https://api.github.com',
     url: `/user`,
     headers: {
       'Authorization': `token ${token}`
