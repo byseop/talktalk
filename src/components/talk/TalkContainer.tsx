f4ef5d152151f210c50becf214e40b502a91a7f6
byseop <byseop@gmail.com>
2020-02-16 18:30:38 +0900

    HOTFIX: Fix login module2

--- src/components/talk/TalkContainer.tsx
+++ src/components/talk/TalkContainer.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react';
 import Talk from './Talk';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'src/modules';
+import { alreadyLogin } from 'src/modules/login';
 
 export default function TalkContainer() {
   const user = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
 
   const { data } = user;
+
+  useEffect(() => {
+    // User information is already stored on the localStorage
+    // Dispatch the data to reducer module.
+    const localStorageUser = window.localStorage.getItem('lUser');
+    if (localStorageUser) {
+      dispatch(alreadyLogin(JSON.parse(localStorageUser)));
+    }
+  }, [dispatch]);
 
   if (data) {
     return <Talk user={data} />;
