7bfc91b48dffa7ad8c39eb9a9805840befbe3c3a
byseop <byseop@gmail.com>
2020-02-24 14:25:34 +0900

    Add Firebase SDK

--- src/components/talk/TalkContainer.tsx
+++ src/components/talk/TalkContainer.tsx
@@ -3,6 +3,27 @@ import Talk from './Talk';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'src/modules';
 import { alreadyLogin } from 'src/modules/login';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_SENDER_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: `${REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: REACT_APP_FIREBASE_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 export default function TalkContainer() {
   const user = useSelector((state: RootState) => state.user);
@@ -16,6 +37,7 @@ export default function TalkContainer() {
     const localStorageUser = window.localStorage.getItem('lUser');
     if (localStorageUser) {
       dispatch(alreadyLogin(JSON.parse(localStorageUser)));
+      firebase.initializeApp(firebaseConfig);
     } else {
       window.location.href = '/';
     }
